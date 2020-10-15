c5d4ef57c14112b2d2a0b21089f38ff080c9816c
Aldric <Aldric@192.168.1.14>
2020-10-15 16:48:41 +0200

    finish class handling server connection and sending messages

--- src/process/connection/ServerConnectionHandler.java
+++ src/process/connection/ServerConnectionHandler.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package process.connection;
 
 import java.io.BufferedReader;
@@ -12,6 +9,7 @@ import java.net.UnknownHostException;
 
 import org.apache.log4j.Logger;
 
+import data.Protocol;
 import logger.LoggerUtility;
 
 /**
@@ -49,6 +47,44 @@ public class ServerConnectionHandler {
 		inputFlow = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		outputFlow = new PrintWriter(socket.getOutputStream(), true);
 		logger.info("Successful server connection !");
+	}
+	
+	/**
+	 * Close the connection with the server. <b>This method must be called before exiting app</b>. 
+	 * @return If connection is closed properly or not
+	 */
+	public boolean closeConnection() {
+		try{
+			outputFlow.close();
+			inputFlow.close();
+			socket.close();
+			return true;
+		}catch (IOException e) {
+			logger.error("Cannot close connection properly : " + e.getMessage());
+			return false;
+		}
+	}
+	
+	/**
+	 * Send a protocol to the server and wait for the answer.
+	 * @param protcol the protocol containing data to process
+	 * @return the string that server send back
+	 * @throws IOException if cannot read what server answered
+	 */
+	public String sendProtocolMessage(Protocol protcol) throws IOException{
+		logger.info("Send message to server.");
+		
+		//create the string of the protocol
+		String protocolString = protcol.toString();
+		
+		//send the message
+		outputFlow.println(protcol.toString());
+		
+		//wait for recieving the server answer
+		/*#readline throws IOException if cannot read from server*/ 
+		String answer = inputFlow.readLine();
+		
+		return answer;
 	}
 
 }
